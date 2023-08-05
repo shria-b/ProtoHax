77294a19a2bb3be3f04f815174a718f2f94d435b
MoonQ <139429525+MoonQ123@users.noreply.github.com>
2023-08-06 01:52:44 +0800

    Update ModuleManager.kt

--- src/main/java/dev/sora/relay/cheat/module/ModuleManager.kt
+++ src/main/java/dev/sora/relay/cheat/module/ModuleManager.kt
@@ -3,50 +3,56 @@ package dev.sora.relay.cheat.module
 import dev.sora.relay.cheat.module.impl.combat.*
 import dev.sora.relay.cheat.module.impl.misc.*
 import dev.sora.relay.cheat.module.impl.movement.*
-import dev.sora.relay.cheat.module.impl.visual.ModuleAntiBlind
-import dev.sora.relay.cheat.module.impl.visual.ModuleHitEffect
-import dev.sora.relay.cheat.module.impl.visual.ModuleNoHurtCam
+import dev.sora.relay.cheat.module.impl.visual.*
 import dev.sora.relay.game.GameSession
 
 class ModuleManager(private val session: GameSession) {
 
-    val modules = mutableListOf<CheatModule>()
+	val modules = mutableListOf<CheatModule>()
 
-    fun registerModule(module: CheatModule) {
+	fun registerModule(module: CheatModule) {
 		module.session = session
 		module.moduleManager = this
-        modules.add(module)
-        module.register(session.eventManager)
-    }
+		modules.add(module)
+		module.register(session.eventManager)
+	}
 
-    fun init() {
-        registerModule(ModuleFly())
-        registerModule(ModuleVelocity())
-        registerModule(ModuleKillAura())
-        registerModule(ModuleSpammer())
-        registerModule(ModuleBGM())
-        registerModule(ModuleDisabler())
-        registerModule(ModuleOpFightBot())
-        registerModule(ModuleNoSkin())
-        registerModule(ModuleDeviceSpoof())
-        registerModule(ModuleResourcePackSpoof())
-        registerModule(ModuleTargets())
-        registerModule(ModuleNoFall())
-        registerModule(ModuleAntiBlind())
-        registerModule(ModuleFastBreak())
-        registerModule(ModuleBlink())
-        registerModule(ModuleBlockFly())
-        registerModule(ModuleInventoryHelper())
-		registerModule(ModuleAirJump())
-		registerModule(ModuleClip())
-		registerModule(ModuleNoHurtCam())
+	fun init() {
+		//combat
+		registerModule(ModuleVelocity())
+		registerModule(ModuleKillAura())
+		registerModule(ModuleTargets())
 		registerModule(ModuleSurround())
 		registerModule(ModuleCrystalAura())
-		registerModule(ModuleHitEffect())
-		registerModule(ModuleMiner())
-		registerModule(ModuleSpeed())
 		registerModule(ModuleHitbox())
-    }
+
+		//misc
+		registerModule(ModuleSpammer())
+		registerModule(ModuleBGM())
+		registerModule(ModuleDisabler())
+		registerModule(ModuleNoSkin())
+		registerModule(ModuleDeviceSpoof())
+		registerModule(ModuleResourcePackSpoof())
+		registerModule(ModuleMiner())
+		registerModule(ModuleInventoryHelper())
+
+		//movement
+		registerModule(ModuleFly())
+		registerModule(ModuleNoFall())
+		registerModule(ModuleFastBreak())
+		registerModule(ModuleBlink())
+		registerModule(ModuleBlockFly())
+		registerModule(ModuleAirJump())
+		registerModule(ModuleClip())
+		registerModule(ModuleSpeed())
+		registerModule(ModuleOpFightBot())
+
+		//visual
+		registerModule(ModuleNoHurtCam())
+		registerModule(ModuleAntiBlind())
+		registerModule(ModuleHitEffect())
+
+	}
 
 	inline fun <reified T : CheatModule> getModule(t: Class<T>): T {
 		return modules.filterIsInstance<T>().first()
