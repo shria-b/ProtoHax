43a2238c05f0e536553e57618dc16967a0c7a450
MoonQ <moonq233@163.com>
2023-09-02 02:21:00 +0800

    Update ModuleGodMode.kt

--- src/main/java/dev/sora/relay/cheat/module/impl/combat/ModuleGodMode.kt
+++ src/main/java/dev/sora/relay/cheat/module/impl/combat/ModuleGodMode.kt
@@ -5,15 +5,17 @@ import dev.sora.relay.cheat.module.CheatModule
 import dev.sora.relay.cheat.value.Choice
 import dev.sora.relay.game.event.EventPacketOutbound
 import dev.sora.relay.game.event.EventTick
+import org.cloudburstmc.protocol.bedrock.data.inventory.transaction.InventoryTransactionType
+import org.cloudburstmc.protocol.bedrock.packet.InventoryTransactionPacket
 import org.cloudburstmc.protocol.bedrock.packet.MovePlayerPacket
 import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket
 
 
 class ModuleGodMode : CheatModule("GodMode", CheatCategory.COMBAT) {
-	private var modeValue by choiceValue("Mode", arrayOf(Packet(), Packet2()), "Packet")
+	private var modeValue by choiceValue("Mode", arrayOf(Packet(), Packet2()), "SendPacket")
 	private var highValue by intValue("High", 5, -20..20)
 
-	private inner class Packet : Choice("Packet"){
+	private inner class Packet : Choice("SendPacket"){
 		private val onTick = handle<EventTick> {
 			session.sendPacket(MovePlayerPacket().apply {
 				runtimeEntityId = session.player.runtimeEntityId
@@ -26,15 +28,20 @@ class ModuleGodMode : CheatModule("GodMode", CheatCategory.COMBAT) {
 		}
 	}
 
-	private inner class Packet2 : Choice("Packet2"){
+	private inner class Packet2 : Choice("CatchPacket"){
 		private var authValue by boolValue("AuthPacket", false)
 		private val onPacketOutbound = handle<EventPacketOutbound> {
-			if(packet is MovePlayerPacket){
-				packet.position = packet.position.add(0f, highValue.toFloat(), 0f)
-			}
-			if(authValue){
-				if(packet is PlayerAuthInputPacket){
-					packet.position = packet.position.add(0f, highValue.toFloat(), 0f)
+			val attackPacket = this.packet
+			if (attackPacket is InventoryTransactionPacket && attackPacket.transactionType == InventoryTransactionType.ITEM_USE_ON_ENTITY) {
+				val movePacket = this.packet
+				if (movePacket is MovePlayerPacket) {
+					movePacket.position = session.player.vec3Position.add(0f, highValue.toFloat(), 0f)
+				}
+				if (authValue) {
+					val authPacket = this.packet
+					if (authPacket is PlayerAuthInputPacket) {
+						authPacket.position = session.player.vec3Position.add(0f, highValue.toFloat(), 0f)
+					}
 				}
 			}
 		}
