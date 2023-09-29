1c9e2dbb234b175d312cc425977a4c9682da2a06
MoonQ <moonq233@163.com>
2023-09-29 16:47:52 +0800

    Protocol v618

--- src/main/java/dev/sora/relay/session/listener/RelayListenerAutoCodec.kt
+++ src/main/java/dev/sora/relay/session/listener/RelayListenerAutoCodec.kt
@@ -37,48 +37,49 @@ import org.cloudburstmc.protocol.bedrock.codec.v575.Bedrock_v575
 import org.cloudburstmc.protocol.bedrock.codec.v582.Bedrock_v582
 import org.cloudburstmc.protocol.bedrock.codec.v589.Bedrock_v589
 import org.cloudburstmc.protocol.bedrock.codec.v594.Bedrock_v594
+import org.cloudburstmc.protocol.bedrock.codec.v618.Bedrock_v618
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket
 import org.cloudburstmc.protocol.bedrock.packet.LoginPacket
 import org.cloudburstmc.protocol.bedrock.packet.RequestNetworkSettingsPacket
 
 class RelayListenerAutoCodec(private val session: MinecraftRelaySession) : MinecraftRelayPacketListener {
 
-    override fun onPacketOutbound(packet: BedrockPacket): Boolean {
-        if (packet is RequestNetworkSettingsPacket) {
-            val codec = pickProtocolCodec(packet.protocolVersion)
-            session.codec = codec
-            logInfo("selected codec (clientProtocol=${packet.protocolVersion}, protocol=${codec.protocolVersion}, mc=${codec.minecraftVersion})")
-        } else if (packet is LoginPacket) {
-            val codec = pickProtocolCodec(packet.protocolVersion)
-            session.codec = codec
-            logInfo("selected codec (clientProtocol=${packet.protocolVersion}, protocol=${codec.protocolVersion}, mc=${codec.minecraftVersion})")
-        }
-        return true
-    }
+	override fun onPacketOutbound(packet: BedrockPacket): Boolean {
+		if (packet is RequestNetworkSettingsPacket) {
+			val codec = pickProtocolCodec(packet.protocolVersion)
+			session.codec = codec
+			logInfo("selected codec (clientProtocol=${packet.protocolVersion}, protocol=${codec.protocolVersion}, mc=${codec.minecraftVersion})")
+		} else if (packet is LoginPacket) {
+			val codec = pickProtocolCodec(packet.protocolVersion)
+			session.codec = codec
+			logInfo("selected codec (clientProtocol=${packet.protocolVersion}, protocol=${codec.protocolVersion}, mc=${codec.minecraftVersion})")
+		}
+		return true
+	}
 
-    companion object {
-        private val protocols = arrayOf(
-            Bedrock_v291.CODEC, Bedrock_v313.CODEC, Bedrock_v332.CODEC,
-            Bedrock_v340.CODEC, Bedrock_v354.CODEC, Bedrock_v361.CODEC,
-            Bedrock_v388.CODEC, Bedrock_v389.CODEC, Bedrock_v390.CODEC,
-            Bedrock_v407.CODEC, Bedrock_v408.CODEC, Bedrock_v419.CODEC,
-            Bedrock_v422.CODEC, Bedrock_v428.CODEC, Bedrock_v431.CODEC,
-            Bedrock_v440.CODEC, Bedrock_v448.CODEC, Bedrock_v471.CODEC,
-            Bedrock_v475.CODEC, Bedrock_v448.CODEC, Bedrock_v465.CODEC,
-            Bedrock_v471.CODEC, Bedrock_v475.CODEC, Bedrock_v486.CODEC,
-            Bedrock_v503.CODEC, Bedrock_v527.CODEC, Bedrock_v534.CODEC,
-            Bedrock_v544.CODEC, Bedrock_v557.CODEC, Bedrock_v560.CODEC,
-            Bedrock_v567.CODEC, Bedrock_v575.CODEC, Bedrock_v582.CODEC,
-			Bedrock_v589.CODEC, Bedrock_v594.CODEC
-        ).associateBy { it.protocolVersion }
+	companion object {
+		private val protocols = arrayOf(
+			Bedrock_v291.CODEC, Bedrock_v313.CODEC, Bedrock_v332.CODEC,
+			Bedrock_v340.CODEC, Bedrock_v354.CODEC, Bedrock_v361.CODEC,
+			Bedrock_v388.CODEC, Bedrock_v389.CODEC, Bedrock_v390.CODEC,
+			Bedrock_v407.CODEC, Bedrock_v408.CODEC, Bedrock_v419.CODEC,
+			Bedrock_v422.CODEC, Bedrock_v428.CODEC, Bedrock_v431.CODEC,
+			Bedrock_v440.CODEC, Bedrock_v448.CODEC, Bedrock_v471.CODEC,
+			Bedrock_v475.CODEC, Bedrock_v448.CODEC, Bedrock_v465.CODEC,
+			Bedrock_v471.CODEC, Bedrock_v475.CODEC, Bedrock_v486.CODEC,
+			Bedrock_v503.CODEC, Bedrock_v527.CODEC, Bedrock_v534.CODEC,
+			Bedrock_v544.CODEC, Bedrock_v557.CODEC, Bedrock_v560.CODEC,
+			Bedrock_v567.CODEC, Bedrock_v575.CODEC, Bedrock_v582.CODEC,
+			Bedrock_v589.CODEC, Bedrock_v594.CODEC, Bedrock_v618.CODEC
+		).associateBy { it.protocolVersion }
 
-        private fun pickProtocolCodec(version: Int): BedrockCodec {
-            var codecResult = BedrockCompat.CODEC
-            for ((ver, codec) in protocols) {
-                if (ver > version) break
-                codecResult = codec
-            }
-            return codecResult
-        }
-    }
+		private fun pickProtocolCodec(version: Int): BedrockCodec {
+			var codecResult = BedrockCompat.CODEC
+			for ((ver, codec) in protocols) {
+				if (ver > version) break
+				codecResult = codec
+			}
+			return codecResult
+		}
+	}
 }
