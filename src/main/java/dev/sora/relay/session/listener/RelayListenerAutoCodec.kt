7a3dc2f67c88e99b3a2c9b8fe5d739626f057f57
cjqnsh0u <102038913+cjqnsh0u@users.noreply.github.com>
2024-02-08 00:43:34 +0800

    Add files via upload

--- src/main/java/dev/sora/relay/session/listener/RelayListenerAutoCodec.kt
+++ src/main/java/dev/sora/relay/session/listener/RelayListenerAutoCodec.kt
@@ -40,7 +40,7 @@ import org.cloudburstmc.protocol.bedrock.codec.v594.Bedrock_v594
 import org.cloudburstmc.protocol.bedrock.codec.v618.Bedrock_v618
 import org.cloudburstmc.protocol.bedrock.codec.v622.Bedrock_v622
 import org.cloudburstmc.protocol.bedrock.codec.v630.Bedrock_v630
-import org.cloudburstmc.protocol.bedrock.codec.v649.Bedrock_v649
+import org.cloudburstmc.protocol.bedrock.codec.v630.Bedrock_v649
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket
 import org.cloudburstmc.protocol.bedrock.packet.LoginPacket
 import org.cloudburstmc.protocol.bedrock.packet.RequestNetworkSettingsPacket
