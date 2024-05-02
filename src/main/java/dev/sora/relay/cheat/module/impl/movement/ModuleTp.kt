ec34555798b825dafb353203e71ee258273cebd1
shria-b <159120355+shria-b@users.noreply.github.com>
2024-05-02 12:26:47 +0800

    Update ModuleTp.kt

--- src/main/java/dev/sora/relay/cheat/module/impl/movement/ModuleTp.kt
+++ src/main/java/dev/sora/relay/cheat/module/impl/movement/ModuleTp.kt
@@ -1,14 +1,14 @@
 package dev.Sora.relay.cheat.module.impl.movement;
 
-import dev.Sora.relay.cheat.module.impl.CheatCategory;
-import dev.Sora.relay.cheat.module.impl.CheatModule;
-import org.cloudburstmc.math.vector.Vector3d;
+import dev.Sora.relay.cheat.module.impl.CheatCategory
+import dev.Sora.relay.cheat.module.impl.CheatModule
+import org.cloudburstmc.math.vector.Vector3d
 
 class ModuleTp: CheatCategory("Tp",CheatCategory,canToggle=false){
   private val x by stringValue("x");
   private val y by stringValue("y");
   private val z by stringValue("z");
-  override fun onEnable{
+  override fun onEnable() {
     if(!session.netSessionInitialized) reture
     
     val player = session.player
